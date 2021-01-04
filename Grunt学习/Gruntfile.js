ec918a99665ce52b410b9b3c96fa9150f8ab6a62
LYT <1504536293@qq.com>
2021-01-04 13:35:23 +0800

    concat基本测试

--- Grunt学习/Gruntfile.js
+++ Grunt学习/Gruntfile.js
@@ -3,20 +3,20 @@ module.exports = function(grunt) {
   //初始化配置grunt任务
   // Project configuration.
   grunt.initConfig({
-    concat: {
+    concat: { //任务名称
       options: {
         separator: ';',
       },
       dist: {
-        src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
-        dest: 'dist/built.js',
+        src: ['src/js/*.js'],
+        dest: 'build/js/build.js',
       },
     },
   });
 
   //当grunt任务执行的时候去加载对应的任务插件
   // 加载包含 "uglify" 任务的插件。
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   //注册grunt的默认任务
   // 默认被执行的任务列表。
