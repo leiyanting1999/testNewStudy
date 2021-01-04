b1c8eb9eb168498961015a06bfb52cec7d2f15ed
LYT <1504536293@qq.com>
2021-01-04 13:49:04 +0800

    js压缩工具的第一次尝试使用

--- Grunt学习/Gruntfile.js
+++ Grunt学习/Gruntfile.js
@@ -12,11 +12,24 @@ module.exports = function(grunt) {
         dest: 'build/js/build.js',
       },
     },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'build/js/build.min.js': ['build/js/build.js']
+        }
+      }
+    }
+
   });
 
   //当grunt任务执行的时候去加载对应的任务插件
   // 加载包含 "uglify" 任务的插件。
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   //注册grunt的默认任务
   // 默认被执行的任务列表。
